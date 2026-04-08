12a9d588e090695a1e6996276b89805de77a7e3b
Deepesh <161422752+deepu4402020@users.noreply.github.com>
2026-04-09 01:07:09 +0530

    Time: 0 ms (100%), Space: 14.8 MB (97.91%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,37 +1,13 @@
 class Solution {
-private:
-    int n;
-    int memo[5001][2];
-
-    int solve(vector<int>& prices, int i, int holding) {
-        if (i >= n) return 0;
-
-        if (memo[i][holding] != -1) return memo[i][holding];
-
-        int profit = 0;
-
-
-        if (holding == 0) {
-            int skip = solve(prices, i + 1, 0);
-            int buy  = -prices[i] + solve(prices, i + 1, 1);
-
-            profit = max(skip, buy);
-        }
-
-        else {
-            int hold = solve(prices, i + 1, 1);
-            int sell = prices[i] + solve(prices, i + 2, 0);
-
-            profit = max(hold, sell);
-        }
-
-        return memo[i][holding] = profit;
-    }
-
 public:
     int maxProfit(vector<int>& prices) {
-        n = prices.size();
-        memset(memo, -1, sizeof(memo));
-        return solve(prices, 0, 0);
+         int buy = -prices[0], sell = 0, cool = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            int newBuy = max(buy, cool - prices[i]);
+            int newSell = max(sell, buy + prices[i]);
+            int newCool = max(cool, sell);
+            buy = newBuy; sell = newSell; cool = newCool;
+        }
+        return max(sell, cool);
     }
 };
